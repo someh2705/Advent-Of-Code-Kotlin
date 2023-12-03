6906cf2814e9b57d17144be46eb70c88fb0bcbbe
someh2705 <someh2705@gmail.com>
2023-12-03 15:51:12 +0900

    solve day3 part two

--- Year2023/src/main/kotlin/advent/of/code/year2023/Day03.kt
+++ Year2023/src/main/kotlin/advent/of/code/year2023/Day03.kt
@@ -2,23 +2,54 @@ package advent.of.code.year2023
 
 object Day03 : Problem(Day03::class) {
 
+    private data class Part(val value: Int, val outline: List<Int>)
+
     fun partOne(path: String) = multiLine(path) { input ->
         val length = input.indexOf('\n') + 1
 
         Regex("\\d+")
             .findAll(input)
-            .filter { validPart(input, it, length) }
-            .sumOf { it.value.toInt() }
+            .map { createPart(it, length) }
+            .filter { validPart(it, input) }
+            .sumOf { it.value }
     }
 
-    private fun validPart(input: String, part: MatchResult, length: Int): Boolean {
+    fun partTwo(path: String) = multiLine(path) { input ->
+        val length = input.indexOf('\n') + 1
+
+        val parts = Regex("\\d+")
+            .findAll(input)
+            .map { createPart(it, length) }
+            .toList()
+
+        val symbols = Regex("[*]")
+            .findAll(input)
+            .map { it.range.first }
+
+        symbols.sumOf { symbol ->
+            val find = parts.filter { it.outline.contains(symbol) }
+
+            if (find.size == 2) {
+                find.first().value * find.last().value
+            } else 0
+        }
+    }
+
+    private fun createPart(part: MatchResult, length: Int): Part {
+
         val range = part.range
         val outline =
             ((range.first - length - 1)..(range.last - length + 1)).toList() +
             ((range.first - 1)..(range.last + 1)).toList() +
             ((range.first + length - 1)..(range.last + length + 1)).toList()
 
-        return outline
+        return Part(part.value.toInt(), outline)
+    }
+
+    private fun validPart(part: Part, input: String): Boolean {
+
+        return part
+                .outline
                 .asSequence()
                 .map { input.getOrNull(it) }
                 .filterNotNull()
--- Year2023/src/test/kotlin/Day03Test.kt
+++ Year2023/src/test/kotlin/Day03Test.kt
@@ -9,12 +9,17 @@ class Day03Test : DescribeSpec({
         it("part one answer is 4361") {
             Day03.partOne("example01.txt") shouldBe 4361
         }
+
+        it("part two answer is 467835") {
+            Day03.partTwo("example01.txt") shouldBe 467835
+        }
     }
 
     describe("compute solution") {
         it("solution not asserted") {
             Debugger.solution {
                 inspect(Day03::partOne, "problem.txt")
+                inspect(Day03::partTwo, "problem.txt")
             } shouldBe Unit
         }
     }
